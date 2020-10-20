2144c71c5235e9f7f99c92819bc49aefb220555e
DariusB2 <darius.boncila@yahoo.com>
2020-10-20 15:05:06 +0300

    Tema4

--- BoncilăDariusLorenzo/L04/Question.Domain/CreateQuestionWorkflow/CreateQuestionCmd.cs
+++ BoncilăDariusLorenzo/L04/Question.Domain/CreateQuestionWorkflow/CreateQuestionCmd.cs
@@ -1,14 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
 
-namespace QuestionBoundedContext.QuestionContext
+namespace Question.Domain.CreateQuestionWorkflow
 {
     public struct CreateQuestionCmd
     {
-        [Required]
         public string Title { get; private set; }
-        [Required]
         public string Description { get; private set; }
-        [Required]
         public string Tags { get; private set; }
 
         public CreateQuestionCmd(string Title, string Description, string Tags)
--- BoncilăDariusLorenzo/L04/Question.Domain/CreateQuestionWorkflow/CreateQuestionResult.cs
+++ BoncilăDariusLorenzo/L04/Question.Domain/CreateQuestionWorkflow/CreateQuestionResult.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
-namespace QuestionBoundedContext.QuestionContext
+namespace Question.Domain.CreateQuestionWorkflow
 {
-    [AsChoice]
     public static partial class CreateQuestionResult
     {
         public interface ICreateQuestionResult { }
@@ -25,12 +25,24 @@ namespace QuestionBoundedContext.QuestionContext
 
         public class QuestionNotCreated : ICreateQuestionResult
         {
+            public string Reason { get; set; }
+
+            public QuestionNotCreated(string reason)
+            {
+                Reason = reason;
+            }
+        }
+    
+        public class QuestionValidationFailed : ICreateQuestionResult
+        {
             public IEnumerable<string> ValidationErrors { get; private set; }
+
             public QuestionValidationFailed(IEnumerable<string> errors)
             {
                 ValidationErrors = errors.AsEnumerable();
             }
-        }
+        } 
 
     }
 }
+
