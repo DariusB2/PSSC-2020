e50cb3c5faf0a1e00e3ec631fab5faa2b54aee97
DariusB2 <darius.boncila@yahoo.com>
2021-01-03 22:06:34 +0200

    Proiect

---
+++ BoncilăDariusLorenzo/Proiect/StackUnderflow/Samples/StackUnderflow.Core/Contexts/Question/CreateQuestion/CreateQuestionCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace StackUnderflow.Domain.Core.Contexts.Question.CreateQuestion
+{
+    public struct CreateQuestionCommand
+    {
+        public CreateQuestionCommand(int questionId, string title, string body, string tags)
+        {
+            QuestionId = questionId;
+            Title = title;
+            Body = body;
+            Tags = tags;
+        }
+        [Required]
+        public int QuestionId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Body { get; set; }
+        [Required]
+        public string Tags { get; set; }
+    }
+}
---
+++ BoncilăDariusLorenzo/Proiect/StackUnderflow/Samples/StackUnderflow.EF.Models/DatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace StackUnderflow.EF
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext()
+        {
+
+        }
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+            : base(options)
+        {
+        }
+    }
+}
